298629b34276a04d2a593fb7dd0045873c951111
leonardoAB1 <leonardoacha@hotmail.com>
2026-05-06 06:44:33 +0200

    chore: configure next.config with mdx plugin and image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+
+const withMDX = createMDX({});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["ts", "tsx", "mdx"],
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+    ],
+  },
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
